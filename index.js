f02247579ed964e68e24bf74948f7f9a38734f08
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2023-08-07 12:46:38 +0300

    update

--- index.js
+++ index.js
@@ -3,9 +3,32 @@ const form = document.querySelector("form");
 const quantityDecreaseBtn = document.querySelector(".quantity-decrease");
 const quantityIncreaseBtn = document.querySelector(".quantity-increase");
 const quantityDisplay = document.querySelector(".quantity-display");
+const productSelect = document.getElementById("products");
+const productTypeSelect = document.getElementById("product_types"); // Add this line
 
 // Initial quantity value
 let quantity = 1;
+
+// Product types data
+const productTypes = {
+  "Hand & Skin Disinfectant": [
+    "Hand & Skin Disinfectant 125ml",
+    "Hand & Skin Disinfectant 750ml",
+  ],
+  "Multi Surface Disinfectant": [
+    "Multi surface Dis 1 L",
+    "Multi surface Dis 5 L",
+    "Multi surface Dis 10 L",
+  ],
+  "Air Disinfectant": ["Air Disinfectant 5 L", "Air Disinfectant 10 L"],
+  "Pet Disinfectant": ["Pet Disinfectant 1 L", "Pet Disinfectant 5 L"],
+  "Fruits & Vegetable Disinfectant": [
+    "Fruits & Vegetable Disinfectant 1 L",
+    "Fruits & Vegetable Disinfectant 5 L",
+  ],
+
+  // Add more product types as needed
+};
 
 // Event listener for decreasing quantity
 quantityDecreaseBtn.addEventListener("click", () => {
@@ -22,6 +45,23 @@ quantityIncreaseBtn.addEventListener("click", () => {
   quantityDisplay.textContent = `Quantity: ${quantity}`;
 });
 
+// Event listener for updating product types
+productSelect.addEventListener("change", () => {
+  const selectedProduct = productSelect.value;
+  const types = productTypes[selectedProduct] || [];
+
+  // Clear existing options
+  productTypeSelect.innerHTML = '<option value="">Select Product Type</option>';
+
+  // Add new options based on selected product
+  types.forEach((type) => {
+    const option = document.createElement("option");
+    option.value = type;
+    option.textContent = type;
+    productTypeSelect.appendChild(option);
+  });
+});
+
 // Form submission handler
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
@@ -30,20 +70,24 @@ form.addEventListener("submit", async (event) => {
   const firstName = document.getElementById("first_name").value;
   const phoneNumber = document.getElementById("phone_number").value;
   const province = document.getElementById("countries").value;
-  const product = document.getElementById("products").value;
+  const product = productSelect.value; // Update this line
+  const productType = productTypeSelect.value; // Add this line
 
   if (
     firstName &&
     phoneNumber &&
     province !== "Select Province" &&
-    product !== "Select Product"
+    product !== "Select Product" &&
+    productType !== "Select Product Type" // Add this condition
   ) {
     const formData = {
       firstName,
       phoneNumber,
       province,
       product,
+      productType, // Add this line
       quantity,
+      price: quantity * 100, // Update this line
     };
 
     // Replace 'YOUR_USER_ID' with your EmailJS user ID
